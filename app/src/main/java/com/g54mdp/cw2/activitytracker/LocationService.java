329afca1898b2d7261f1e529219a22dd54fcb470
Raul Regalado <rrs2712@gmail.com>
2016-12-20 14:27:34 +0000

    LocationService - Service added (LocationService) - LocationManager implementation moved from MainActivity
    Icons
    - Notification bar icon added (multisizes)
    - Launch icon added (multisizes)
    
    MainActivity
    - LocationManager implementation moved to LocationService
    - Implementation of use of LocationService, including biding
    
    activity_main
    - start button added
    - stop button added

---
+++ app/src/main/java/com/g54mdp/cw2/activitytracker/LocationService.java
@@ -0,0 +1,148 @@
+package com.g54mdp.cw2.activitytracker;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class LocationService extends Service {
+    private final String
+        CLA = "RRS LocationService",
+        MSG = "Tracking in background",
+        TITLE = "Activity Tracker",
+        CONTENT = "is tracking";
+
+    private final IBinder binder = new LocationServiceBinder();
+
+    private LocationManager locationManager;
+    private LocationListenerImpl locationListener;
+
+    private final long MIN_TIME_LOC_UPDATES_MILLI_SECS = 5;
+    private final float MIN_DIST_LOC_UPDATES_METERS = 5;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(CLA, "onBind");
+
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(CLA, "onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(CLA, "onDestroy");
+        super.onDestroy();
+
+        stopLocationManager();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(CLA, "onStartCommand");
+
+        startLocationManager();
+
+        return Service.START_STICKY;
+    }
+
+
+    public void createNotification(){
+        Log.d(CLA,"createNotification");
+
+        Intent mainActivity = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,mainActivity,0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+            .setTicker(MSG)
+            .setSmallIcon(R.drawable.ic_notification_bar)
+            .setContentTitle(TITLE)
+            .setContentText(CONTENT)
+            .setContentIntent(pi)
+            .setAutoCancel(true) // Notification is automatically canceled when the user clicks it in the panel
+            .setOngoing(true) //  Sorted above the regular notifications in the notification panel and do not have an 'X' close button, and are not affected by the "Clear all" button
+            .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notification);
+    }
+
+    private void stopLocationManager() {
+        Log.d(CLA,"stopLocationManager");
+
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(CLA,"Location permissions denied, can't stop LocationManager");
+            return;
+        }
+
+        try {
+            locationManager.removeUpdates(locationListener);
+        } catch (Exception e) {
+            Log.w(CLA,e.toString());
+        }
+    }
+
+    private void startLocationManager() {
+        Log.d(CLA,"startLocationManager");
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListenerImpl();
+
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(CLA,"Location permissions denied, can't start LocationManager");
+            return;
+        }
+
+        try {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    MIN_TIME_LOC_UPDATES_MILLI_SECS,
+                    MIN_DIST_LOC_UPDATES_METERS,
+                    locationListener
+            );
+        } catch (Exception e) {
+            Log.w(CLA,e.toString());
+        }
+    }
+
+    public class LocationServiceBinder extends Binder {
+        LocationService getService(){
+            return LocationService.this;
+        }
+    }
+}
--- app/src/main/java/com/g54mdp/cw2/activitytracker/MainActivity.java
+++ app/src/main/java/com/g54mdp/cw2/activitytracker/MainActivity.java
@@ -1,22 +1,24 @@
 package com.g54mdp.cw2.activitytracker;
 
-import android.Manifest;
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String CLA = "RRS MainActivity";
-    private final long MIN_TIME_LOC_UPDATES_MILLI_SECS = 5;
-    private final float MIN_DIST_LOC_UPDATES_METERS = 5;
 
-    private LocationManager locationManager;
-    private LocationListenerImpl locationListener;
+    private LocationService service;
+
+    private boolean keepServiceRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,72 +32,91 @@ public class MainActivity extends AppCompatActivity {
         Log.d(CLA, "onStart");
         super.onStart();
 
-        startLocationManager();
+        startAndBindLocationService();
     }
 
     @Override
     protected void onStop() {
         Log.d(CLA, "onStop");
         super.onStop();
-
-        stopLocationManager();
     }
 
-    private void stopLocationManager() {
-        Log.d(CLA,"stopLocationManager");
+    @Override
+    protected void onDestroy() {
+        Log.d(CLA,"onDestroy");
+        super.onDestroy();
 
-        if (ActivityCompat.checkSelfPermission(
-            this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(
-                    this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d(CLA,"Location permissions denied, can't stop LocationManager");
-            return;
-        }
-
-        try {
-            locationManager.removeUpdates(locationListener);
-        } catch (Exception e) {
-            Log.w(CLA,e.toString());
+        if(keepServiceRunning){
+            service.createNotification();
         }
     }
 
-    private void startLocationManager() {
-        Log.d(CLA,"startLocationManager");
+    public void onBtnStart(View view){
+        Log.d(CLA, "onBtnStart");
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListenerImpl();
+//        startAndBindLocationService();
+        Toast.makeText(this,"Now this button does nothing",Toast.LENGTH_SHORT).show();
+    }
 
-        if (ActivityCompat.checkSelfPermission(
-            this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(
-                    this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d(CLA,"Location permissions denied, can't start LocationManager");
-            return;
+    public void onBtnStop(View view){
+        Log.d(CLA, "onBtnStop");
+
+        if(keepServiceRunning){
+            unbindLocationService();
+            stopLocationService();
+        }else{
+            startAndBindLocationService();
         }
 
-        try {
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                MIN_TIME_LOC_UPDATES_MILLI_SECS,
-                MIN_DIST_LOC_UPDATES_METERS,
-                locationListener
-            );
-        } catch (Exception e) {
-            Log.w(CLA,e.toString());
+    }
+
+    private void manageGUI(){
+        Button btn = (Button) findViewById(R.id.btn_stop);
+
+        if(keepServiceRunning){
+            btn.setText("Stop service");
+        }else{
+            btn.setText("Start service");
         }
     }
+
+    private void startAndBindLocationService() {
+        Intent serviceIntent = new Intent(MainActivity.this,LocationService.class);
+        this.startService(serviceIntent);
+        bindLocationService(serviceIntent);
+        keepServiceRunning = true;
+        manageGUI();
+    }
+
+    private void bindLocationService(Intent serviceIntent) {
+        this.bindService(serviceIntent,serviceConnection,Context.BIND_AUTO_CREATE);
+    }
+
+    private void unbindLocationService() {
+        if(serviceConnection!=null){
+            unbindService(serviceConnection);
+        }
+    }
+
+    private void stopLocationService() {
+        Intent serviceIntent = new Intent(MainActivity.this,LocationService.class);
+        this.stopService(serviceIntent);
+        keepServiceRunning = false;
+        manageGUI();
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(CLA, "onServiceConnected");
+            LocationService.LocationServiceBinder binder = (LocationService.LocationServiceBinder) service;
+            MainActivity.this.service = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(CLA,"onServiceDisconnected");
+        }
+    };
+
 }
