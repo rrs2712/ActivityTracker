d1af7202094646cbfc625d7bb4ee87600c539aaf
Raul Regalado <rrs2712@gmail.com>
2017-01-07 23:41:46 +0000

    Layouts - activity_record_details changed - activity_main changed - icons gps_on & gps_off updated
    Classes
    - RecordDetails.java show location marks in google map through an intent added

--- app/src/main/java/com/g54mdp/cw2/activitytracker/RecordDetails.java
+++ app/src/main/java/com/g54mdp/cw2/activitytracker/RecordDetails.java
@@ -1,11 +1,15 @@
 package com.g54mdp.cw2.activitytracker;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -16,11 +20,15 @@ public class RecordDetails extends AppCompatActivity {
 
     private final String CLA = "RRS RecordDetails";
     private final String NUMERIC_FORMAT="###,###.##";
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    private GoogleApiClient client;
+    private final String MAP_URL = "http://maps.google.com/maps";
+    private final String MAP_ADDRESS_1 = "?saddr=";
+    private final String MAP_ADDRESS_2 = "&daddr=";
+    private final String MAP_ADDRESS_N = "+to:";
+    private final String MAP_WALKING = "&dirflg=w";
+
+    private final int MAP_MAX_LOCATIONS = 25;
+
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,6 @@ public class RecordDetails extends AppCompatActivity {
         String dayFi = bundle.getString(RecordViewer.TAG_DAY_FILTER);
 
         setGUI(dayFo, dayFi);
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void setGUI(String dayFormatted, String dayFilter) {
@@ -44,7 +49,7 @@ public class RecordDetails extends AppCompatActivity {
         TextView tvSpeed = (TextView) findViewById(R.id.tv_speed);
         TextView tvHeight = (TextView) findViewById(R.id.tv_height);
 
-        Cursor cursor = getInfoFromDay(dayFilter);
+        cursor = getInfoFromDay(dayFilter);
 
         float fDistance = getTotalDistance(cursor);
         float fTime     = getTotalTime(cursor);
@@ -120,14 +125,10 @@ public class RecordDetails extends AppCompatActivity {
                 cursor.moveToFirst();
                 Date date1 = simpleDateFormat.parse(cursor.getString(1));
                 Log.d(CLA,"d1: "+cursor.getString(1));
-//                Calendar calendar1 = Calendar.getInstance();
-//                calendar1.setTime(date1);
 
                 cursor.moveToLast();
                 Date date2 = simpleDateFormat.parse(cursor.getString(1));
                 Log.d(CLA,"d2: "+cursor.getString(1));
-//                Calendar calendar2 = Calendar.getInstance();
-//                calendar2.setTime(date2);
 
                 long d = date2.getTime() - date1.getTime();
                 Log.d(CLA,"diff long: "+ d);
@@ -135,8 +136,7 @@ public class RecordDetails extends AppCompatActivity {
                 float f = (float) d;
                 Log.d(CLA,"diff float: "+ f);
 
-//                min = (f/1000)/60;
-                min = f/60000;
+                min = f/60000; // min = (f/1000)/60;
 
             } catch (ParseException e) {
                 Log.e(CLA,e.getMessage());
@@ -161,7 +161,7 @@ public class RecordDetails extends AppCompatActivity {
                 locationNow.setLatitude(cursor.getDouble(2));
                 locationNow.setLongitude(cursor.getDouble(3));
                 locationNow.setAltitude(cursor.getDouble(4));
-//                Log.d(CLA, "Longitude now " + locationNow.getLongitude());
+//                Log.d(CLA, "Now: LAT=" + locationNow.getLatitude() + "\tLON=" + locationNow.getLongitude());
                 if (locationPre != null) {
 //                    Log.d(CLA, "Longitude pre " + locationPre.getLongitude());
                     distance += locationPre.distanceTo(locationNow);
@@ -172,11 +172,6 @@ public class RecordDetails extends AppCompatActivity {
 
                 cursor.moveToNext();
             }
-////                toLog += cursor.getString(0); toLog += " | "; //id
-////                toLog += cursor.getString(1); toLog += " | "; //date
-////                toLog += cursor.getString(2); toLog += " | "; //lat
-////                toLog += cursor.getString(3); toLog += " | "; //lon
-////                toLog += cursor.getString(4); toLog += "\n"; //alt
         }
 
         Log.d(CLA, "Total distance (mts) = " + distance);
@@ -196,39 +191,89 @@ public class RecordDetails extends AppCompatActivity {
         return getContentResolver().query(ProviderContract.LOCATION_URI_ID, projection, day, null, null);
     }
 
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    public Action getIndexApiAction() {
-//        Thing object = new Thing.Builder()
-//                .setName("RecordDetails Page") // TODO: Define a title for the content shown.
-//                // TODO: Make sure this auto-generated URL is correct.
-//                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-//                .build();
-//        return new Action.Builder(Action.TYPE_VIEW)
-//                .setObject(object)
-//                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-//                .build();
-//    }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
+    public void onMap(View view){
+
+        if (isInfoEnoughToShowMap()){
+            String myMap = getUrlFollowedRoute();
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(myMap));
+            startActivity(i);
+        }else{
+            Toast.makeText(this,"Not enough info to show a map",Toast.LENGTH_LONG).show();
+        }
+
+
+//        String loc1 = "52.935476666666666,-1.2099366666666667";
+//        String loc2 = "52.93706,-1.2152183333333333";
+//        String loc3 = "52.94745833333333,-1.18347";
+//        String loc4 = "52.93431999999999,-1.20717";
 //
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-//        client.disconnect();
-//    }
+//        String navigationUrl = "http://maps.google.com/maps?saddr="+loc1+"&daddr="+loc2+"+to:"+loc3+"+to:"+loc4;
+//        Intent navIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(navigationUrl));
+//        startActivity(navIntent);
+
+    }
+
+    private String getUrlFollowedRoute(){
+
+        String url = MAP_URL;
+        int recordCounter = 0;
+
+//        if (cursor.getCount()>MAP_MAX_LOCATIONS){
+            recordCounter = (int)(cursor.getCount()/MAP_MAX_LOCATIONS);
+            String locations[] = new String[MAP_MAX_LOCATIONS];
+//            Log.d(CLA,"Cursor=" + cursor.getCount() + "\tMax loc=" + MAP_MAX_LOCATIONS + "\tFactor=" + recordCounter);
+
+//            if (cursor.moveToFirst()) {
+
+            cursor.moveToFirst();
+//            Log.d(CLA, "First: " + cursor.getDouble(2) + "   " + cursor.getDouble(3));
+            locations[0] = cursor.getDouble(2) + "," + cursor.getDouble(3);
+
+            int i = 1;
+            int moveCursorTo = recordCounter;
+            while (i<=(MAP_MAX_LOCATIONS-2)){
+//                try {
+//                    Log.d(CLA,"i=" + i + " moveCursorTo=" + moveCursorTo);
+                    cursor.moveToFirst();
+                    cursor.move(moveCursorTo);
+//                    Log.d(CLA, "" + i + ":" + cursor.getDouble(2) + "   " + cursor.getDouble(3));
+                    locations[i] = cursor.getDouble(2) + "," + cursor.getDouble(3);
+                    moveCursorTo += recordCounter;
+                    i++;
+//                } catch (Exception e) {
+//                    Log.d(CLA,e.getMessage());
+//                    break;
+//                }
+            }
+
+            cursor.moveToLast();
+//            Log.d(CLA, "Last: " + cursor.getDouble(2) + "   " + cursor.getDouble(3));
+            locations[MAP_MAX_LOCATIONS-1] = cursor.getDouble(2) + "," + cursor.getDouble(3);
+
+            for (int a=0; a<locations.length;a++){
+                Log.d(CLA, "" + a + " = " + locations[a]);
+                switch (a){
+                    case 0:
+                        url += MAP_ADDRESS_1 + locations[0];
+                        break;
+                    case 1:
+                        url += MAP_ADDRESS_2 + locations[1];
+                        break;
+                    default:
+                        url += MAP_ADDRESS_N + locations[a];
+                }
+            }
+
+        url += MAP_WALKING;
+//            }
+//        }
+        Log.d(CLA,url);
+
+        return  url;
+    }
+
+    private boolean isInfoEnoughToShowMap(){
+        return (cursor.getCount() > MAP_MAX_LOCATIONS) ? true : false;
+    }
 }
